ad04610a0654482dc59e0d629c367aa3c52c4096
Kevin Ludlam <kevin_ludlam@bloomfield.edu>
2018-01-26 23:36:15 -0500

    all basic for player in

--- Assets/Scripts/Player_Controler.cs
+++ Assets/Scripts/Player_Controler.cs
@@ -10,34 +10,123 @@ public class Player_Controler : MonoBehaviour {
     public CharacterController cc;
     public float gravty;
     private float yreset;
-
+    private bool pluged;
+    public string gender;
+    public Player_Controler otherPlayer;
+    private bool canPlug;
+   // public bool genm;
     // Use this for initialization
     void Start () {
 
         cc = GetComponent<CharacterController>();
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        yreset = moveValosity.y;
-
-        moveValosity = (transform.right * Input.GetAxisRaw("Horizontal") * playerSpeed);
-
-        moveValosity = moveValosity.normalized * playerSpeed;
-
-        moveValosity.y = yreset;
-
-        if (cc.isGrounded)
+        if(!pluged && gender == "mail" && otherPlayer.pluged == true)
         {
-            moveValosity.y = 0f;
-            if (Input.GetButtonDown("Jump"))
+            yreset = moveValosity.y;
+
+            moveValosity = (transform.right * Input.GetAxisRaw("Horizontal") * playerSpeed);
+
+            moveValosity = moveValosity.normalized * playerSpeed;
+
+            moveValosity.y = yreset;
+
+            if (cc.isGrounded)
             {
-                moveValosity.y = jumpHight;
+                moveValosity.y = 0f;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveValosity.y = jumpHight;
+                }
             }
+
+            moveValosity.y = moveValosity.y + (Physics.gravity.y * gravty * Time.deltaTime);
+            cc.Move(moveValosity * Time.deltaTime);
+
+            if(Input.GetButtonDown("Fire1") && canPlug)
+            {
+                pluged = true;
+                otherPlayer.pluged = false;
+            }
+
+
         }
 
-        moveValosity.y = moveValosity.y + (Physics.gravity.y * gravty * Time.deltaTime);
-        cc.Move(moveValosity * Time.deltaTime);
+        if (!pluged && gender == "femail" && otherPlayer.pluged == true)
+        {
+            yreset = moveValosity.y;
+
+            moveValosity = (transform.right * Input.GetAxisRaw("Fehorizontal") * playerSpeed);
+
+            moveValosity = moveValosity.normalized * playerSpeed;
+
+            moveValosity.y = yreset;
+
+            if (cc.isGrounded)
+            {
+                moveValosity.y = 0f;
+                if (Input.GetButtonDown("Fejump"))
+                {
+                    moveValosity.y = jumpHight;
+                }
+            }
+
+            moveValosity.y = moveValosity.y + (Physics.gravity.y * gravty * Time.deltaTime);
+            cc.Move(moveValosity * Time.deltaTime);
+
+            if (Input.GetButtonDown("Fefire1") && canPlug)
+            {
+                pluged = true;
+                otherPlayer.pluged = false;
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        
+
+        if (gender == "mail" && other.tag == "mailplug")
+        {
+
+            // pluged = true;
+            // otherPlayer.pluged = false;
+            canPlug = true;
+            
+        }
+
+       
+            
+        if (gender == "femail" && other.tag == "femailplug" )
+        {
+            //pluged = true;
+            // otherPlayer.pluged = false;
+            canPlug = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (gender == "mail" && other.tag == "mailplug")
+        {
+
+            // pluged = true;
+            // otherPlayer.pluged = false;
+            canPlug = false;
+
+        }
+
+
+
+        if (gender == "femail" && other.tag == "femailplug")
+        {
+            //pluged = true;
+            // otherPlayer.pluged = false;
+            canPlug = false;
+        }
     }
 }
