1947427b2e301dd5c72fca9db88e3cd9f37e7b93
kirmanator <davidsk95@yahoo.com>
2018-01-28 13:22:02 -0500

    Added platform movement script

---
+++ Assets/Scripts/PlatformBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehavior : MonoBehaviour {
+
+	public bool activated;
+	private Vector3 startPos, endPos;
+	public bool atStart;
+	public float heightChange;
+	public float speed;
+	private float maxDistanceDelta;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		endPos = new Vector3 (transform.position.x, transform.position.y + heightChange, transform.position.z);
+		maxDistanceDelta = speed * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (activated) {
+			if (atStart) {
+				transform.position = Vector3.MoveTowards (transform.position, endPos, maxDistanceDelta);
+				atStart = false;
+			}
+			else {
+				transform.position = Vector3.MoveTowards (transform.position, startPos, maxDistanceDelta);
+				atStart = true;
+			}
+		}
+	}
+}
